a673c56ccb385308d916fa69d5f986f0b4c336da
Guilherme Frederico Lima <gflogos@gmail.com>
2022-04-15 01:34:15 -0500

    first draft of classifiers.py

--- classifiers.py
+++ classifiers.py
@@ -1,29 +1,29 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.utils.validation import column_or_1d
 
 
-
-def group_select(dataframe, group1, group2):
-    mask = (dataframe['group'] == group1) | (dataframe['group'] == group2)
+def group_select(dataframe, group1, group2, group3):
+    mask = (dataframe['group'] == group1) | (dataframe['group'] == group2) | (dataframe['group'] == group3)
     return dataframe[mask]
 
 
 groupnames = ['control', 'mania', 'mixed mania', 'mixed depression', 'depression', 'euthymia']
 
 df = pd.read_csv("Data/Verbal Tasks joined features/joined_features.csv")
-group1 = 'depression'
-group2 = 'mania'
-df_sub = group_select(df, group1, group2)
+group1 = 'mania'
+group2 = 'depression'
+group3 = group2
+df_sub = group_select(df, group1, group2, group3)
 to_drop = ['Unnamed: 0', 'number', 'group', 'group number']
 df_X = df_sub.drop(to_drop, axis=1)
 df_y = column_or_1d(y=df_sub[['group number']], warn=False)
 
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, random_state=0)
 
-linsvc = LinearSVC().fit(X_train, y_train)
-print("Training set score: {:.2f}".format(linsvc.score(X_train, y_train)))
-print("Test set score: {:.2f}".format(linsvc.score(X_test, y_test)))
+svc = SVC(kernel='rbf', C=1, gamma=0.1).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(svc.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(svc.score(X_test, y_test)))
 #print("Number of features used:", np.sum(logreg.coef_ != 0))
