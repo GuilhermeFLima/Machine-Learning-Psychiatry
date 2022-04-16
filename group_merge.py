a883f11e35b7b58ede9b7f71d88d5dcdeaab7471
Guilherme Frederico Lima <gflogos@gmail.com>
2022-04-16 02:40:39 -0500

    refactored group_merge.py
    Now has if name == main.

--- group_merge.py
+++ group_merge.py
@@ -21,9 +21,13 @@ def group_num_merge(dataframe, group_num1: int, group_num2: int):
     return df
 
 
+def name_merge(name1: str, name2: str):
+    return name1 + ' & ' + name2
+
+
 def group_name_merge(dataframe, group_name1: str, group_name2: str):
     df = dataframe
-    merged_name = group_name1 + ' & ' + group_name2
+    merged_name = name_merge(group_name1, group_name2)
     df['group'] = df['group'].map(lambda group_name: group_swap(group_name, group_name1, merged_name))
     df['group'] = df['group'].map(lambda group_name: group_swap(group_name, group_name2, merged_name))
     return df
@@ -37,12 +41,14 @@ def group_merge(dataframe, group_name1: str, group_name2: str):
     df = group_name_merge(df, group_name1, group_name2)
     return df
 
-testdf = pd.read_csv("Data/Verbal Tasks joined features/joined_features.csv")
-print(testdf.head(10))
-group1 = 'mania'
-group2 = 'mixed mania'
-df_merged = group_merge(testdf, group1, group2)
-print(df_merged[['group', 'group number']].head(20))
+
+if __name__ == "__main__":
+    testdf = pd.read_csv("Data/Verbal Tasks joined features/joined_features.csv")
+    print(testdf.head(10))
+    group1 = 'mania'
+    group2 = 'mixed mania'
+    df_merged = group_merge(testdf, group1, group2)
+    print(df_merged[['group', 'group number']].head(20))
 
 
 
