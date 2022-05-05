d0caac17fa27b6e4377c76fe5707948eef6dbbf3
Guilherme Frederico Lima <gflogos@gmail.com>
2022-05-04 21:36:34 -0500

    removed avg_anch_sim from feature extract

--- feature_extract.py
+++ feature_extract.py
@@ -1,5 +1,4 @@
 import get_vec as gv
-import anchor_similarity as anchsim
 import neighbour_similarity as neighsim
 import global_similarity as globalsim
 import word_repetitions as wordrep
@@ -17,61 +16,21 @@ def get_number(filename: str) -> str:
     return list_with_number[0]
 
 
-anchor_list = ['courage', 'debut', 'douleur', 'piscine', 'royaume', 'serpent']
-anchor_cols = ['number', 'group', 'unique_entries', 'repeat_entries', 'repeat_words', 'avg_anch_sim', 'avg_global_sim', 'avg_neigh_sim']
-simple_fluence_list = ['fcat', 'flib', 'flit']
-simple_cols = ['number', 'group', 'unique_entries', 'repeat_entries', 'repeat_words', 'avg_global_sim', 'avg_neigh_sim']
+tasks = ['courage', 'debut', 'douleur', 'piscine', 'royaume', 'serpent', 'fcat', 'flib', 'flit']
+feature_cols = ['number', 'group', 'unique_entries', 'repeat_entries', 'repeat_words', 'avg_global_sim', 'avg_neigh_sim']
 
 
-def anchor_features():
+def features():
     path = "Data/Verbal Tasks 1-3/"
     newpath = "Data/Verbal Tasks features/"
     allfiles = listdir(path)
     csvfiles = sorted([x for x in allfiles if '.csv' in x])
 
-    for anchor in anchor_list:
-        print(anchor)
+    for task in tasks:
+        print(task)
         list_of_features = []
         for file in csvfiles:
-            if anchor in file:
-                try:
-                    path_name = path + file
-                    df = pd.read_csv(path_name)
-                    s = df['0']
-                    number = get_number(file)
-                    group = groups.number_to_group(number)
-                    s_vec = gv.vec_series(s)
-                    neigh_sim = neighsim.avg_neighbour_sim(s_vec)
-                    anch_word = anchsim.get_anchor(path_name)
-                    anch_vec = gv.get_vec(anch_word)
-                    anch_sim = anchsim.avg_anchor_sim(anchor_vec=anch_vec, series=s_vec)
-                    s_unique = gv.vec_series_unique(s)
-                    global_sim = globalsim.global_sim(s_unique)
-                    unique_entries = len(s_unique)
-                    repeat_entries = s.duplicated().sum()
-                    repeat_words = wordrep.word_repeat(s)
-                    arr = np.array([number, group, unique_entries, repeat_entries, repeat_words, anch_sim, global_sim, neigh_sim])
-                    list_of_features.append(arr)
-
-                except Exception as e:
-                    print(e)
-                    print('problem with:', file)
-        df = pd.DataFrame(list_of_features, columns=anchor_cols)
-        new_filename = anchor + '_features.csv'
-        df.to_csv(newpath+new_filename)
-
-
-def simple_features():
-    path = "Data/Verbal Tasks 1-3/"
-    newpath = "Data/Verbal Tasks features/"
-    allfiles = listdir(path)
-    csvfiles = sorted([x for x in allfiles if '.csv' in x])
-
-    for fluence in simple_fluence_list:
-        print(fluence)
-        list_of_features = []
-        for file in csvfiles:
-            if fluence in file:
+            if task in file:
                 try:
                     path_name = path + file
                     df = pd.read_csv(path_name)
@@ -91,14 +50,14 @@ def simple_features():
                 except Exception as e:
                     print(e)
                     print('problem with:', file)
-        df = pd.DataFrame(list_of_features, columns=simple_cols)
-        new_filename = fluence + '_features.csv'
-        df.to_csv(newpath + new_filename)
+        df = pd.DataFrame(list_of_features, columns=feature_cols)
+        new_filename = task + '_features.csv'
+        df.to_csv(newpath+new_filename, index=False)
 
 
 if __name__ == '__main__':
-    anchor_features()
-    simple_features()
+    features()
+
 
 
 
