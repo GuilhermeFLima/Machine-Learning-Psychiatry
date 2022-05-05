e0e57c69794cc94490d30180c322c4fdc202396e
Guilherme Frederico Lima <gflogos@gmail.com>
2022-05-04 21:34:10 -0500

    removed index from _features.csv files
    deleted old task_join

--- task_join.py
+++ task_join.py
@@ -1,45 +1,71 @@
 import pandas as pd
 
-anchor_list = ['courage', 'debut', 'douleur', 'piscine', 'royaume', 'serpent']
-simple_fluence_list = ['fcat', 'flib', 'flit']
-total_tasks = anchor_list + simple_fluence_list
 
-filename1 = "Data/Verbal Tasks features/courage_features.csv"
-filename2 = "Data/Verbal Tasks features/debut_features.csv"
-filename3 = "Data/Verbal Tasks features/douleur_features.csv"
-filename4 = "Data/Verbal Tasks features/piscine_features.csv"
-filename5 = "Data/Verbal Tasks features/royaume_features.csv"
-filename6 = "Data/Verbal Tasks features/serpent_features.csv"
-filename7 = "Data/Verbal Tasks features/fcat_features.csv"
-filename8 = "Data/Verbal Tasks features/flib_features.csv"
-filename9 = "Data/Verbal Tasks features/flit_features.csv"
-
-df1 = pd.read_csv(filename1)
-df1.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df2 = pd.read_csv(filename2)
-df2.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df3 = pd.read_csv(filename3)
-df3.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df4 = pd.read_csv(filename4)
-df4.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df5 = pd.read_csv(filename5)
-df5.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df6 = pd.read_csv(filename6)
-df6.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df7 = pd.read_csv(filename7)
-df7.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df8 = pd.read_csv(filename8)
-df8.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
-df9 = pd.read_csv(filename9)
-df9.drop(['Unnamed: 0', 'number', 'group'], axis=1, inplace=True)
+def features_dataframes(files_list: list) -> list:
+    """
+    Given the list of csv files, returns the list of dataframes, and removes
+    the columns 'number' and 'group', since these will be added later.
+    """
+    path = "Data/Verbal Tasks features/"
+    files = [path + file for file in files_list]
+    dataframes = [pd.read_csv(file) for file in files]
+    for df in dataframes:
+        df.drop(['number', 'group'], axis=1, inplace=True)
+    return dataframes
 
 
-df0 = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9], axis=1, keys=total_tasks)
-df0.columns = ['_'.join(col) for col in df0.columns.values]
+def join_dataframes(dataframes: list):
+    """
+    Joins the dataframes and renames the columns, since these were double-indexed.
+    """
+    total_tasks = ['courage', 'debut', 'douleur', 'piscine',
+                   'royaume', 'serpent', 'fcat', 'flib', 'flit']
+    joined = pd.concat(dataframes, axis=1, keys=total_tasks)
+    joined.columns = ['_'.join(col) for col in joined.columns.values]
+    return joined
 
-df_num_group = pd.read_csv(filename1)
-df_num_group = df_num_group[['number', 'group']]
-groupnames = ['control', 'mania', 'mixed mania', 'mixed depression', 'depression', 'euthymia']
-df_num_group['group number'] = df_num_group['group'].map(lambda n: groupnames.index(n))
-df = pd.concat([df_num_group, df0], axis=1)
-df.to_csv('Data/Verbal Tasks joined features/joined_features.csv')
+
+def group_number(group: str) -> int:
+    """
+    Given a group name, returns it's corresponding number, ie:
+    control -> 0
+    mania -> 1
+    etc...
+    """
+    groupnames = ['control',
+                  'mania',
+                  'mixed mania',
+                  'mixed depression',
+                  'depression',
+                  'euthymia']
+    return groupnames.index(group)
+
+
+def number_group_groupnumber():
+    """
+    Returns a dataframe with the patient number, its group, and the group's number.
+    """
+    file = "Data/Verbal Tasks features/courage_features.csv"
+    df = pd.read_csv(file)
+    df = df[['number', 'group']]
+    df['group number'] = df['group'].map(group_number)
+    return df
+
+
+if __name__ == '__main__':
+    csv_files = ['courage_features.csv',
+                 'debut_features.csv',
+                 'douleur_features.csv',
+                 'piscine_features.csv',
+                 'royaume_features.csv',
+                 'serpent_features.csv',
+                 'fcat_features.csv',
+                 'flib_features.csv',
+                 'flit_features.csv']
+    dfs = features_dataframes(files_list=csv_files)
+    joined_tasks = join_dataframes(dataframes=dfs)
+    df0 = number_group_groupnumber()
+    final_dataframe = pd.concat([df0, joined_tasks], axis=1)
+    final_dataframe.to_csv('Data/Verbal Tasks joined features/joined_features2.csv', index=False)
+
+
